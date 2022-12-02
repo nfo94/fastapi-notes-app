7e7ea45407feb293addb9e750653c2fa83dbc79b
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-12-02 07:24:13 -0300

    feat: add post route

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.api import healthcheck
+from app.api import healthcheck, notes
 from app.db import engine, database, metadata
 
 metadata.create_all(engine)
@@ -19,33 +19,4 @@ async def shutdown():
 
 
 app.include_router(healthcheck.router)
-
-
-# @app.get("/healthcheck")
-# async def healthcheck():
-#     return {"healthcheck": "ok"}
-
-
-# @app.get("/notes")
-# async def get_notes():
-#     return {"notes": "notes"}
-
-
-# @app.get("/notes/{id}")
-# async def get_note():
-#     return {"note": "note"}
-
-
-# @app.post("/notes")
-# async def post_note():
-#     return {"note": "note"}
-
-
-# @app.put("/notes/{id}")
-# async def put_note():
-#     return {"note": "note"}
-
-
-# @app.delete("/notes/{id}")
-# async def delete_note():
-#     return {"note": "note"}
+app.include_router(notes.router, prefix="/notes", tags=["notes"])
