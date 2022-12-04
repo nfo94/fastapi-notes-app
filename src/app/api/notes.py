80540662c059e3b1a6c2dfc77e45f4f8b5389dd4
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-12-04 06:39:53 -0300

    feat: add get all notes route

--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, HTTPException
 
 from app.api import crud
@@ -19,9 +20,14 @@ async def create_note(payload: NoteSchema):
     return response_object
 
 
-@router.get("/{id}")
+@router.get("/{id}", response_model=NoteDB)
 async def read_note(id: int):
     note = await crud.get(id)
     if not note:
         raise HTTPException(status_code=404, detail="Note not found")
     return note
+
+
+@router.get("/", response_model=List[NoteDB])
+async def read_all_notes():
+    return await crud.get_all()
